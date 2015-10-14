95f561ba794b718a01c5d6ab23de708849855fb2
Hackbright Student <info@hackbrightacademy.com>
2015-10-14 13:14:18 -0700

    added route

--- hackbright-web.py
+++ hackbright-web.py
@@ -22,29 +22,25 @@ def get_student_form():
     return render_template("student_search.html")
 
 
-
-
-
-
 @app.route("/show-student-add-form")
 def student_add():
-    hackbright.make_new_student(first_name, last_name, github)
     """Add a student."""
-
+    hackbright.make_new_student(first_name, last_name, github)
+    
     return  render_template("student-add.html")
 
 
 @app.route("/add-student")
 def student_add():
 
-    first_name = request.form.args("firstname")
-    last
-    GitHub
+    first_name = request.form.args("firstname") # passing on name value from form to get the data
+    last_name = request.form.args("lastname")
+    GitHub = request.form.args("new_github")
 
-    hackbright.make_new_student(first_name, last_name, github)
+    hackbright.make_new_student(first_name, last_name, github) # from hackbright.py file
     """Add a student."""
 
-    return  render_template("success")
+    return  render_template()
 
 
 if __name__ == "__main__":
